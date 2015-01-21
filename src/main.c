1e43e020a7b03a0207c67245bcf8455bdcb2e120
Michael Savage <mikejsavage@gmail.com>
2015-01-21 10:07:00 +0000

    Bump year

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Michael Savage <mike@mikejsavage.co.uk>
+ * Copyright (c) 2015, Michael Savage <mike@mikejsavage.co.uk>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
