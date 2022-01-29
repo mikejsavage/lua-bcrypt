b54466a8a38f7d807660d5bd129a805876c43f7f
Michael Savage <mikejsavage@gmail.com>
2022-01-29 11:28:37 +0000

    formatting squash

--- src/safebfuns.c
+++ src/safebfuns.c
@@ -16,7 +16,7 @@ void explicit_bzero( void * buf, size_t n ) {
 	volatile char * volbuf = ( volatile char * ) buf;
 
 	size_t i;
-	for (i = 0; i < n; i++) {
+	for( i = 0; i < n; i++ ) {
 		volbuf[ i ] = 0;
 	}
 }
@@ -27,7 +27,7 @@ int timingsafe_bcmp( const void * b1, const void * b2, size_t n ) {
 	int result = 0;
 
 	size_t i;
-	for (i = 0; i < n; i++) {
+	for( i = 0; i < n; i++ ) {
 		result |= p1[ i ] ^ p2[ i ];
 	}
 
